a434e402aa7f80763b5cae83b86551a61c1ceac6
rushikesh <rushikeshsunilganesh@gmail.com>
2024-01-08 23:15:53 +0530

    new changes

---
+++ src/api/todos/todos.route.ts
@@ -0,0 +1,11 @@
+import {Router , Request ,Response} from 'express';
+import {Todo,TodoWithId,Todos} from './todos.model';
+const router = Router();
+
+router.get('/',async (req:Request,res:Response<TodoWithId[]>)=>{
+    const result = await Todos.find();
+    const todos =await result.toArray();
+    res.json(todos);
+})
+
+export default router;
